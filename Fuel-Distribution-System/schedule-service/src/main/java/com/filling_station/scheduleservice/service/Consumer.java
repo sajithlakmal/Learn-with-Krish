9a16eb592a9731a5661d2845bf429156dab021ef
“sajithlakmal” <sajithandlakmal100@gmail.com>
2022-09-11 21:54:33 +0530

    Consumer added

--- Fuel-Distribution-System/schedule-service/src/main/java/com/filling_station/scheduleservice/service/Consumer.java
+++ Fuel-Distribution-System/schedule-service/src/main/java/com/filling_station/scheduleservice/service/Consumer.java
@@ -1,4 +1,18 @@
 package com.filling_station.scheduleservice.service;
 
+import com.filling_station.orderservice.model.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
 public class Consumer {
+
+
+    @KafkaListener(topics = "allocation-topic", groupId = "allocation-group")
+
+    public void listenAllocationTopic(Order order) {
+        System.out.println("Listen to allocation topic in AllocationService from OrderService " + order);
+
+    }
 }
